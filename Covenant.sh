8f5e3b6e0e3b947a160f2ae4a28e23a9702676cd
Iron Hulk <65414049+projectHULK@users.noreply.github.com>
2022-12-21 17:43:59 +0400

    Add files via upload
    I have created one-liner installation for the C2 frameworks, you my also visit:
    * Havoc: https://github.com/HavocFramework/Havoc
    * Covenant: https://github.com/cobbr/Covenant/

---
+++ Covenant.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+#------------------> Color Code:
+RED="\033[01;31m"
+BLUE="\033[36m"
+GREEN="\033[01;32m"
+XX="\033[0m" #--- Close COLOR
+
+echo -e "\n${RED}╔═════{ Run as root user${XX}"
+sleep 0.5
+echo -e "\n${BLUE}╔═════{ Clone the repository:${XX}"
+	git clone --recurse-submodules https://github.com/cobbr/Covenant
+echo -e "\n${GREEN}    ══{ Installation: ${XX}"
+	cd Havoc/Client && make  && ./Havoc
+echo -e "\n${BLUE}╔═════{ Building the Teamserver:${XX}"
+sleep 0.5
+	cd Covenant/Covenant && dotnet run
+echo -e "\n${GREEN}╔═════{ Installation Completed${XX}"
+echo -e "\n${BLUE}╔═════{ Open the like:${XX}"
+echo -e "\n${GREEN}    ══{ https://127.0.0.1:7443/covenantuser/login${XX}"
+echo -e "\n${BLUE}╔═════{ Read More at:${XX}"
+echo -e "\n${GREEN}    ══{ https://github.com/cobbr/Covenant/ ${XX}"
+
+
---
+++ havoc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+#------------------> Color Code:
+RED="\033[01;31m"
+BLUE="\033[36m"
+GREEN="\033[01;32m"
+XX="\033[0m" #--- Close COLOR
+
+echo -e "\n${RED}╔═════{ Run as root user${XX}"
+sleep 0.5
+echo -e "\n${BLUE}╔═════{ Installation:${XX}"
+echo -e "\n${GREEN}    ══{ Pre-requisites: ${XX}"
+	apt install -y git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go qtbase5-dev libqt5websockets5-dev libspdlog-dev python3-dev libboost-all-dev mingw-w64 nasm
+echo -e "\n${GREEN}    ══{ Python: ${XX}"
+	apt install python3-dev python3.10-dev libpython3.10 libpython3.10-dev python3.10
+echo -e "\n${BLUE}╔═════{ Building the Client:${XX}"
+sleep 0.5
+echo -e "\n${GREEN}    ══{ Clone the repository: ${XX}"
+	git clone https://github.com/HavocFramework/Havoc.git 
+echo -e "\n${GREEN}    ══{ Clone the repository: ${XX}"
+	cd Havoc/Client && make  && ./Havoc
+echo -e "\n${BLUE}╔═════{ Building the Teamserver:${XX}"
+sleep 0.5
+	cd .. && cd Teamserver && go mod download golang.org/x/sys && go mod download github.com/ugorji/go && ./Install.sh && make
+echo -e "\n${GREEN}╔═════{ Installation Completed${XX}"
+echo -e "\n${BLUE}╔═════{ Start Havoc by doing the following:${XX}"
+echo -e "\n${GREEN}    ══{ Start Teamserver: cd Havoc/Teamserver && ./teamserver server --profile ./profiles/havoc.yaotl ${XX}"
+echo -e "\n${GREEN}    ══{ Start Havoc in other terminal: cd Havoc/Client && ./Havoc ${XX}"
+echo -e "\n${GREEN}    ══{ The username and password are set to defualt: 5pider/password1234 ${XX}"
+echo -e "\n${GREEN}    ══{ that can be changed from the config file: Havoc/Teamserver/profiles/havoc_default.yaotl ${XX}"
+
+
