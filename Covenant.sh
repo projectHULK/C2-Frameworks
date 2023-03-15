625a6162a6af3419bcee7530036d5ecc3b3eaae2
Iron Hulk <65414049+projectHULK@users.noreply.github.com>
2023-03-15 13:15:28 +0400

    Update Covenant.sh

--- Covenant.sh
+++ Covenant.sh
@@ -9,16 +9,16 @@ XX="\033[0m" #--- Close COLOR
 
 echo -e "\n${RED}╔═════{ Run as root user${XX}"
 sleep 0.5
+echo -e "\n${BLUE}╔═════{ Installing .NET:${XX}"
+	wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh && chmod +x ./dotnet-install.sh && ./dotnet-install.sh --version latest
 echo -e "\n${BLUE}╔═════{ Cloning the repository:${XX}"
 	git clone --recurse-submodules https://github.com/cobbr/Covenant
 sleep 0.5
 	cd Covenant/Covenant && dotnet run
 echo -e "\n${GREEN}╔═════{ Installation Completed${XX}"
-echo -e "\n${BLUE}╔═════{ Run the framework :${XX}"
+echo -e "\n${BLUE}╔═════{ Go to Covenant/Covenant & Run the framework:${XX}"
 echo -e "\n${GREEN}    ══{ dotnet run${XX}"
 echo -e "\n${BLUE}╔═════{ Open the link:${XX}"
 echo -e "\n${GREEN}    ══{ https://127.0.0.1:7443/covenantuser/login${XX}"
 echo -e "\n${BLUE}╔═════{ Read More at:${XX}"
 echo -e "\n${GREEN}    ══{ https://github.com/cobbr/Covenant/ ${XX}"
-
-
